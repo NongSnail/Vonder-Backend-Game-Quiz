766c14adae096128a82a4e9270e4e691ea71c08f
NongFai <narisara.li@mail.kmutt.ac.th>
2021-02-22 03:21:20 +0700

    create class

--- app/app.js
+++ app/app.js
@@ -1,59 +1,114 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-}) 
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// }) 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// })
 
-app.post('/', function(req, res){
-    res.send('Got a POST request')
-})
+// app.post('/', function(req, res){
+//     res.send('Got a POST request')
+// })
 
-app.put('/user',function(req, res){
-    res.send('Got a PUT request at /user')
-})
+// app.put('/user',function(req, res){
+//     res.send('Got a PUT request at /user')
+// })
 
-app.delete('/user',function(req, res){
-    res.send('Got a DELETE request at /user')
-})
+// app.delete('/user',function(req, res){
+//     res.send('Got a DELETE request at /user')
+// })
 
 
-// class Books {
-//     constructor(id, title, description, category, price) {
-//         this.id = id;
-//         this.title = title;
-//         this.description = description;
-//         this.category = category;
-//         this.price = price;
-//     }
+class Books {
+    constructor(id, title, description, category, price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+    }
 
-//     get getId(){
-//         this.id = id;
-//     }
+    get getId(){
+        return this.id;
+    }
 
-//     get getTitle(){
-//         this.title = title;
-//     }
+    get getTitle(){
+        return this.title;
+    }
 
-//     get getDescription(){
-//         this.description = description;
-//     }
+    get getDescription(){
+        return this.description;
+    }
 
-//     get getCategory(){
-//         this.category = category;
-//     }
+    get getCategory(){
+        return this.category;
+    }
 
-//     get getPrice(){
-//         this.price = price;
-//     } 
-// }
+    get getPrice(){
+        return this.getPrice;
+    } 
 
+    
+}
+
+var book = new Books('854-59-0591','Ice Princess','imperdiet et commodo vulputate justo in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin'
+,'Music','$0.78');
+
+
+
+console.log(book);
+
+class Shelf {
+    
+    constructor(){
+        this.shelf = [];
+    }
+
+    add(book) {
+        this.shelf.push(book);
+    }
+
+    delete(book) {
+        var bookName = book.getTitle;
+        this.shelf.forEach( (e,i) => {
+            if(e === bookName ){
+                this.shelf.splice(i)
+            }
+        }
+        )
+    }
+
+
+}
+
+class Book_Shelf {
+    constructor(){
+        this.Book_Shelf = [];
+    }
+    add(shelf) {
+        this.Book_Shelf.push(shelf);
+    }
+
+    delete(shelf) {
+        this.Book_Shelf.splice(shelf);
+    }
+
+    search(){
+        var bookName = book.getTitle;
+        this.shelf.forEach( (e,i) => {
+            if(e === bookName ){
+                console.log(e);
+                console.log(i);
+            }
+        }
+        )
+    }
+}
