d3283e2c9cc27f43dd18ff432cbed54b95b52a2c
NongFai <narisara.li@mail.kmutt.ac.th>
2021-02-21 18:21:28 +0700

    test express routing

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.post('/', function(req, res){
+    res.send('Got a POST request')
+})
+
+app.put('/user',function(req, res){
+    res,send('Got a PUT request at /user')
+})
+
+app.delete('/user',function(req, res){
+    res.send('Got a DELETE request at /user')
+})
 
 
 // class Books {
